11fd69e1fc7954cf128e705c0587d769f5ee2bc6
Kimball Bradford <kimball.bradford@gmail.com>
2018-11-20 17:57:27 +0100

    front end works

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from project.api.views.helloworld import HelloWorld
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HelloWorld.as_view())
+    path('', include('project.api.urls'))
 ]
