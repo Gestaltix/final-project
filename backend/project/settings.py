f1c78f5044d06705e379987ce5a28460b67e4228
Kimball Bradford <kimball.bradford@gmail.com>
2018-11-20 11:11:46 +0100

    first commit

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -121,4 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+MEDIA_ROOT = "/media-files"
+MEDIA_URL = "/media/"
+STATIC_URL = "/static/"
+STATIC_ROOT = "/static-files"
