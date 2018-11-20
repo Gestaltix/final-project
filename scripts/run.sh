6957b56d17cbebb7e3146bdf67dc5586a4883e0a
Kimball Bradford <kimball.bradford@gmail.com>
2018-11-20 10:51:12 +0100

    first commit

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
-python manage.py migrate
-python manage.py collectstatic --noinput
-python manage.py runserver 0.0.0.0:8000
+##!/bin/bash
+#python manage.py migrate
+#python manage.py collectstatic --noinput
+#python manage.py runserver 0.0.0.0:8000
 #exec /opt/conda/envs/app/bin/uwsgi --ini /scripts/uwsgi.ini
 #/opt/conda/envs/app/bin/flask db upgrade
 #exec /opt/conda/envs/app/bin/gunicorn -w 4 -b 0.0.0.0:8000 app:app
