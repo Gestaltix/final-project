da3c3e03991b3682258234d378d99170a7f93a16
Kimball Bradford <kimball.bradford@gmail.com>
2018-11-21 11:51:06 +0100

    front end works

--- backend/project/api/urls.py
+++ backend/project/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from api.views.helloworld import HelloWorld
+from project.api.views.helloworld import HelloWorld
 
 urlpatterns = [
     path('', HelloWorld.as_view(), name='hello-world')
