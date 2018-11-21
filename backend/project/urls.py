b54cdd19b1b6282799a0e037283ce20a809e43e2
Kimball Bradford <kimball.bradford@gmail.com>
2018-11-21 10:43:29 +0100

    front end works

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('project.api.urls'))
+    path('', include('project.api.urls')),
+
+    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
