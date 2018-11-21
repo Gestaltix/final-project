acb9cd6fab618ef8b74ee16dffd931a32bc82d84
Kimball Bradford <kimball.bradford@gmail.com>
2018-11-21 14:04:40 +0100

    front end works

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -19,7 +19,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('project.api.urls')),
+    path('backend/', include('project.api.urls')),
 
     path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
