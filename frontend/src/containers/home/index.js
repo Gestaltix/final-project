f1a3df897c631c5da6d6c9ae6fc09d71f2b19430
Kimball Bradford <kimball.bradford@gmail.com>
2018-11-26 09:47:00 +0100

    Figuring out data_type issues

--- frontend/src/containers/home/index.js
+++ frontend/src/containers/home/index.js
@@ -6,6 +6,7 @@ import connection from '../../connection';
 import { FilePond } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
 import FilePondPluginFileEncode from 'filepond-plugin-file-encode';
+import Plot from 'react-plotly.js';
 
 class Home extends Component {
   render() {
@@ -16,6 +17,31 @@ class Home extends Component {
       </div >
     );
   }
+  // render() {
+  //   return (
+  //     <div>
+  //       <TopBar />
+  //       <Plot
+  //         data={[
+  //           {
+  //             // x: [1, 2, 3],
+  //             // y: [2, 6, 3],
+  //             // type: 'scatter',
+  //             // mode: 'lines+points',
+  //             // marker: { color: 'red' },
+  //           },
+  //           { type: 'bar', x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], y: [2, 5, 3, 4, 5, 2, 3, 7, 8, 5, 8] },
+  //         ]}
+  //         layout={{ title: 'A Fancy Plot' }}
+  //         style={{
+  //           height: '100%',
+  //           // width: 'auto',
+  //           margin: '0 auto',
+  //         }}
+  //       />
+  //     </div>
+  //   );
+  // }
   onDrop = (files) => {
     console.log(files)
     const data = new FormData()
