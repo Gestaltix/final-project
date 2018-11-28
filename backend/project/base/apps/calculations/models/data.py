3b3dbd780be45514e6a6051340cfcf41cd4fe213
Colin Wirz <cwirz@pyango.ch>
2018-11-28 09:39:20 +0100

    Calculation data for players per second

--- backend/project/base/apps/calculations/models/data.py
+++ backend/project/base/apps/calculations/models/data.py
@@ -1,8 +1,20 @@
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
 
+CATEGORIES = [
+    (-1, 'Low Power \n < 10 W/kg'),
+    (10, 'Intermediate Power\n 10 < x < 20 W/kg'),
+    (20, 'High Power\n 20 < x < 35 W/kg'),
+    (35, 'Enormous Power\n 35 < x < 55 W/kg'),
+    (55, 'Maximum Power\n  55 W/kg <'),
+]
+
 
 class Data(TimeStampedModel):
+    category = models.IntegerField(
+        verbose_name='category',
+        choices=CATEGORIES,
+    )
     session = models.ForeignKey(
         verbose_name='session',
         to='trackers.Session',
@@ -33,7 +45,7 @@ class Data(TimeStampedModel):
     watt_kg = models.FloatField(
         verbose_name='latitude (deg)',
     )
-    load = models.IntegerField(
+    load = models.FloatField(
         verbose_name='player load (abs cumsum / 9.81)',
     )
 
@@ -42,13 +54,6 @@ class Data(TimeStampedModel):
 
 
 class PowerCategroy(TimeStampedModel):
-    CATEGORIES = [
-        (10, 'Low Power \n < 10 W/kg'),
-        (20, 'Intermediate Power\n 10 < x < 20 W/kg'),
-        (35, 'High Power\n 20 < x < 35 W/kg'),
-        (55, 'Enormous Power\n 35 < x < 55 W/kg'),
-        (100, 'Maximum Power\n  55 W/kg <'),
-    ]
     category = models.IntegerField(
         verbose_name='category',
         choices=CATEGORIES,
