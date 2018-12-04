0b5af9e37c82e34921f49a2f48ad07e2809ae4b7
Kimball Bradford <kimball.bradford@gmail.com>
2018-12-04 19:05:05 +0100

    load data button

--- frontend/src/containers/team/index.js
+++ frontend/src/containers/team/index.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 import './index.css'
 import connect from "react-redux/es/connect/connect";
 import TopBar from "../../components/topbar";
+import { ListItem, ListItemText } from '@material-ui/core';
 
 class Team extends Component {
-    render () {
+    selectMember = (memberId) => () => {
+        this.props.history.push(`/player/${memberId}`)
+    }
+    render() {
         return (
             <div>
-                <TopBar history={this.props.history}/>
+                <TopBar history={this.props.history} />
                 <h1>Hello</h1>
                 <h1>{this.props.team.name}</h1>
+                {this.props.team.members ?
+                    this.props.team.members.map(member => {
+                        return <ListItem button key={member.id}>
+                            <ListItemText primary={member.name} onClick={this.selectMember(member.id)} />
+                        </ListItem>
+                    }) : null}
             </div>
         );
     }
